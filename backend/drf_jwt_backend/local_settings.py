fd682434479d9172fd2ab5de211ded962fa35b1d
wisetechlts <wisesioux919221@gmail.com>
2025-06-22 05:07:00 -0500

    Moved working Authentication application from code base. Moved working Cars application from codebase. Moved working drf_jwt_backend folder over from codebase.

---
+++ backend/drf_jwt_backend/local_settings.py
@@ -0,0 +1,9 @@
+SECRET_KEY = 'django-insecure-4!@#%$^&*()_+1234567890qwertyuiopasdfghjklzxcvbnm',
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'db.sqlite3',
+    }
+}
+# SECURITY WARNING: don't run with debug turned on in production!
